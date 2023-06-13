c30406fed19d765bf02a03cfa2fd7e9007717b2b
Simon <carbontwelve@users.noreply.github.com>
2023-06-14 00:44:50 +0100

    feat: handle inertia requests

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Scheduler;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+use Tightenco\Ziggy\Ziggy;
+
+class HandleInertiaRequests extends Middleware
+{
+    /**
+     * The root template that is loaded on the first page visit.
+     *
+     * @var string
+     */
+    protected $rootView = 'app';
+
+    /**
+     * Determine the current asset version.
+     */
+    public function version(Request $request): string|null
+    {
+        return parent::version($request);
+    }
+
+    /**
+     * Define the props that are shared by default.
+     *
+     * @return array<string, mixed>
+     */
+    public function share(Request $request): array
+    {
+        return array_merge(parent::share($request), [
+            'auth' => [
+                'user' => $request->user(),
+            ],
+            'stats' => [
+                'players_online' => User::loggedInCount(),
+                'scheduler_next_run' => Scheduler::nextRun()->unix(),
+                'sched_ticks' => config('scheduler.sched_ticks')
+            ],
+            'ziggy' => function () use ($request) {
+                return array_merge((new Ziggy)->toArray(), [
+                    'location' => $request->url(),
+                ]);
+            },
+        ]);
+    }
+}
