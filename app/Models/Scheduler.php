6a077a9b4d7508418f038ee091db37d413114d59
Simon <carbontwelve@users.noreply.github.com>
2023-06-14 00:44:21 +0100

    feat: add last/next run scheduler helpers

--- app/Models/Scheduler.php
+++ app/Models/Scheduler.php
@@ -2,10 +2,37 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Scheduler extends Model
 {
     use HasFactory;
+
+    /**
+     *
+     * Implements SchedulerGateway::selectSchedulerLastRun
+     * @return Carbon|null
+     */
+    public static function lastRun(): ?Carbon
+    {
+        /** @var Scheduler|null $last */
+        $last = self::query()
+            ->orderBy('last_run', 'DESC')
+            ->first();
+
+        return (is_null($last))
+            ? null
+            : $last->last_run;
+    }
+
+    public static function nextRun(): Carbon
+    {
+        $seconds = config('scheduler.sched_ticks') * 60;
+
+        if (!$lastRun = self::lastRun()) return Carbon::now();
+
+        return $lastRun->addSeconds($seconds);
+    }
 }
