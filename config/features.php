f65f179daff6496624dc7133183670b5a872fdd1
Simon <carbontwelve@users.noreply.github.com>
2023-06-13 22:49:14 +0100

    chore: copy classic_config.ini into laravel config files

---
+++ config/features.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allow Ful Long Range Scanning
+    |--------------------------------------------------------------------------
+    |
+    | Allow players to use full long range scan during this game?
+    |
+    */
+    'allow_fullscan' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allow Navigation Computer
+    |--------------------------------------------------------------------------
+    |
+    | Allow players to use the Navigation computer during this game?
+    |
+    */
+    'allow_navcomp' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allow Banking
+    |--------------------------------------------------------------------------
+    |
+    | Allow players to use the Intergalactic Bank during this game?
+    |
+    */
+    'allow_ibank' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allow Planetary Destruction
+    |--------------------------------------------------------------------------
+    |
+    | Allow players to use genesis torps to destroy planets?
+    |
+    */
+    'allow_genesis_destroy' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allow sofa Attack (???)
+    |--------------------------------------------------------------------------
+    |
+    | Allow the sub-orbital fighter sofa attack in this game?
+    |
+    */
+    'allow_sofa' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allow Known Space Map
+    |--------------------------------------------------------------------------
+    |
+    | Allow the known space map in this game?
+    |
+    */
+    'allow_ksm' => true,
+];
