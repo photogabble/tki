5c0334af8877e238f962449f3db88902d78d6843
Simon <carbontwelve@users.noreply.github.com>
2023-06-15 00:25:25 +0100

    feat: installer creates planets

--- app/Installer/CreatePlanets.php
+++ app/Installer/CreatePlanets.php
@@ -3,18 +3,76 @@
 namespace App\Installer;
 
 // 70.php
+use App\Models\Planet;
+use App\Models\Universe;
+use App\Models\Zone;
 use Illuminate\Console\OutputStyle;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
-class CreatePlanets implements InstallStep{
+class CreatePlanets extends Step implements InstallStep
+{
+    /**
+     * @throws \Exception
+     */
     public function execute(OutputStyle $output, InstallConfig $config): int
     {
-        // Get the sector id for any sector that allows planets
+        $this->timer->start();
 
-        // Insert all of the planets in one mega sql shot
+        // Get the sectors belonging to zones that allows planets
+        /** @var Universe[]|Collection<Universe> $sectors */
+        $sectors = Universe::query()
+            ->inRandomOrder()
+            ->join('zones', 'zone_id', '=', 'zones.id')
+            ->where('zones.allow_planet', true)
+            ->select('universes.*')
+            ->get();
 
-        // Add Sector Size amount of links to the links table
 
-        // Add (sector size * 2) amount of links to the links table
+        $added = 0;
+        $populatedSectors = 0;
+        $default_prod_ore = config('game.default_prod_ore');
+        $default_prod_organics = config('game.default_prod_organics');
+        $default_prod_goods = config('game.default_prod_goods');
+        $default_prod_energy = config('game.default_prod_energy');
+        $default_prod_fighters = config('game.default_prod_fighters');
+        $default_prod_torp = config('game.default_prod_torp');
+
+        // Insert all planets within one transaction
+        DB::beginTransaction();
+
+        while ($added < $config->unownedPlanets) {
+            /** @var Universe $sector */
+            if (!$sector = $sectors->pop()) break; // Run out of sectors... this shouldn't happen but this break stops the infinite loop
+            $adding = random_int(1, config('game.max_planets_sector'));
+
+            // Ensure we don't add more than the total amount needed, if we are on the last loop, add enough
+            // to complete the list.
+            if ($adding + $added > $config->unownedPlanets) $adding = $config->unownedPlanets - $added;
+
+            for ($i = 0; $i < $adding; $i++) {
+                $planet = new Planet();
+                $planet->colonists = 2;
+                $planet->owner = null; // default
+                $planet->team = null; // default
+                $planet->prod_ore = $default_prod_ore;
+                $planet->prod_organics = $default_prod_organics;
+                $planet->prod_goods = $default_prod_goods;
+                $planet->prod_energy = $default_prod_energy;
+                $planet->prod_fighters = $default_prod_fighters;
+                $planet->prod_torp = $default_prod_torp;
+                $planet->sector_id = $sector->id;
+                $planet->save();
+
+                $added++;
+            }
+
+            $populatedSectors++;
+        }
+
+        DB::commit();
+
+        $output->writeln(__('create_universe.l_cu_setup_unowned_planets', ['elapsed' => $this->timer->sample(), 'nump' => $populatedSectors]));
 
         return 0;
     }
