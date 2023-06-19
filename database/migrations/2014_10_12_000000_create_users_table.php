e509e7721f5ff75c10622177f75a4c4868e7f486
Simon <carbontwelve@users.noreply.github.com>
2023-06-19 22:59:04 +0100

    refactor: add user attributes moved from ships

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,27 @@ return new class extends Migration
         // Is players table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('team_id')->nullable();
+            $table->unsignedBigInteger('team_invite')->nullable();
+
+            // If null then the player has died in space
+            $table->unsignedBigInteger('ship_id')->nullable();
+
+            $table->string('name', 20);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
 
             $table->timestamp('last_login')->nullable()->default(null);
             $table->string('lang', 30)->default('en_GB');
+
+            $table->unsignedInteger('turns')->default(0);
+            $table->unsignedInteger('turns_used')->default(0);
+
+            $table->unsignedInteger('rating')->default(0);
+            $table->unsignedInteger('score')->default(0);
+
+            $table->unsignedInteger('credits')->default(0);
 
             $table->rememberToken();
             $table->timestamps();
