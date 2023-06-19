133a68749212c8d92e57958b9eb8aa8eefac5857
Simon <carbontwelve@users.noreply.github.com>
2023-06-19 21:32:19 +0100

    refactor: porting php to vue component

--- zoneinfo.php
+++ zoneinfo.php
@@ -1,215 +1,80 @@
-<?php declare(strict_types = 1);
-/**
- * zoneinfo.php from The Kabal Invasion.
- * The Kabal Invasion is a Free & Opensource (FOSS), web-based 4X space/strategy game.
- *
- * @copyright 2020 The Kabal Invasion development team, Ron Harwood, and the BNT development team
- *
- * @license GNU AGPL version 3.0 or (at your option) any later version.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+<script setup lang="ts">
+    /**
+     * zoneinfo.php from The Kabal Invasion.
+     * The Kabal Invasion is a Free & Opensource (FOSS), web-based 4X space/strategy game.
+     *
+     * @copyright 2023 Simon Dann, The Kabal Invasion development team, Ron Harwood, and the BNT development team
+     *
+     * @license GNU AGPL version 3.0 or (at your option) any later version.
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License as
+     * published by the Free Software Foundation, either version 3 of the
+     * License, or (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Affero General Public License for more details.
+     *
+     * You should have received a copy of the GNU Affero General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     *
+     */
 
-require_once './common.php';
+    import ZonePermission from "@/Components/atoms/ZonePermission.vue";
+    import PopUp from "@/Components/atoms/modal/PopUp.vue";
+    import {ZoneInfo} from "@/types/zone-info";
 
-$login = new Tki\Login();
-$login->checkLogin($pdo_db, $lang, $tkireg, $tkitimer, $template);
+    const zone : ZoneInfo = {
+        name: 'Warzone',
+        owner: 'Contested space',
+        isEditable: false,
+        isFriendly: 'L',
+        allow_beacon: 'L',
+        allow_attack: 'Y',
+        allow_planetattack: 'Y',
+        allow_warpedit: 'Y',
+        allow_planet: 'Y',
+        allow_trade: 'N',
+        allow_defenses: 'Y',
+        max_hull: 0,
+        over_size: false,
+    };
 
-// Database driven language entries
-$langvars = Tki\Translate::load($pdo_db, $lang, array('attack', 'common',
-                                'footer', 'insignias', 'main',
-                                'modify_defenses', 'port', 'report',
-                                'universal', 'zoneinfo'));
-$title = $langvars['l_zi_title'];
-$body_class = 'zoneinfo';
+    defineProps<{
+        modelValue: Boolean;
+    }>();
 
-$header = new Tki\Header();
-$header->display($pdo_db, $lang, $template, $title, $body_class);
+    defineEmits(['update:modelValue']);
+</script>
 
-echo "<h1>" . $title . "</h1>\n";
-echo "<body class=" . $body_class . ">";
-$zone = (int) filter_input(INPUT_GET, 'zone', FILTER_SANITIZE_NUMBER_INT);
+<template>
+    <pop-up :show="modelValue" @close="$emit('update:modelValue', false)">
+        <!-- TODO: Add zoneedit.php, ported to Vue -->
 
-// Get playerinfo from database
-$players_gateway = new \Tki\Models\User($pdo_db);
-$playerinfo = $players_gateway->selectPlayerInfo($_SESSION['username']);
+        <div class="p-6 w-3/5 border border-ui-orange-500 bg-ui-grey-900/90 border-x-4">
+            <header class="flex items-center">
+                <h2 class="flex-grow text-lg font-medium text-ui-yellow">Zone Info</h2>
+                <div class="flex space-x-4">
+                    <button v-if="zone.isEditable" class="underline">You can [e]dit this zone</button>
+                    <button @click="$emit('update:modelValue', false)" class="underline">Close [ESC]</button>
+                </div>
+            </header>
+            <p class="mt-1 text-sm text-white">
+                {{ zone.name }}: <span :class="{'text-green-600': zone.isFriendly === 'Y', 'text-ui-orange-500': zone.isFriendly === 'L', 'text-red-600': zone.isFriendly === 'N'}">{{ zone.owner }}</span>.
+            </p>
+            <p class="mt-1 text-sm text-white">
+                {{__('report.l_beacons')}} are <zone-permission :value="zone.allow_beacon" />,
+                Attacking is <zone-permission :value="zone.allow_attack" />, deploying Sector Defenses is
+                <zone-permission :value="zone.allow_defenses" />, Warp Editors are <zone-permission :value="zone.allow_warpedit" />,
+                Planets are <zone-permission :value="zone.allow_planet" /> and Port Trading is <zone-permission :value="zone.allow_trade" />.
+            </p>
 
-// Get zoneinfo from database
-$zones_gateway = new \Tki\Models\Zone($pdo_db);
-$zoneinfo = $zones_gateway->selectZoneInfoByZone($zone);
-$ownerinfo = array();
-
-if (empty($zoneinfo))
-{
-    echo $langvars['l_zi_nexist'];
-}
-else
-{
-    if ($zoneinfo['zone_id'] < 5)
-    {
-        $zonevar = "l_zname_" . $zoneinfo['zone_id'];
-        $zoneinfo['zone_name'] = $langvars[$zonevar];
-    }
-
-    if ($zoneinfo['zone_id'] == '2')
-    {
-        $ownername = $langvars['l_zi_feds'];
-    }
-    elseif ($zoneinfo['zone_id'] == '3')
-    {
-        $ownername = $langvars['l_zi_traders'];
-    }
-    elseif ($zoneinfo['zone_id'] == '1')
-    {
-        $ownername = $langvars['l_zi_nobody'];
-    }
-    elseif ($zoneinfo['zone_id'] == '4')
-    {
-        $ownername = $langvars['l_zi_war'];
-    }
-    else
-    {
-        // Sanitize ZoneName.
-        $zoneinfo['zone_name'] = preg_replace('/[^A-Za-z0-9\_\s\-\.\']+/', '', $zoneinfo['zone_name']);
-
-        if ($zoneinfo['team_zone'] == 'N')
-        {
-            $players_gateway = new \Tki\Models\User($pdo_db);
-            $ownerinfo = $players_gateway->selectPlayerInfoById($zoneinfo['owner']);
-            $ownername = $ownerinfo['character_name'];
-        }
-        else
-        {
-            $sql = "SELECT team_name, creator, id FROM ::prefix::teams WHERE id = :id LIMIT 1";
-            $stmt = $pdo_db->prepare($sql);
-            $stmt->bindParam(':id', $zoneinfo['owner'], PDO::PARAM_INT);
-            $stmt->execute();
-            $ownerinfo = $stmt->fetch(PDO::FETCH_ASSOC);
-            $ownername = $ownerinfo['team_name'];
-        }
-    }
-
-    if ($zoneinfo['allow_beacon'] == 'Y')
-    {
-        $beacon = $langvars['l_zi_allow'];
-    }
-    elseif ($zoneinfo['allow_beacon'] == 'N')
-    {
-        $beacon = $langvars['l_zi_notallow'];
-    }
-    else
-    {
-        $beacon = $langvars['l_zi_limit'];
-    }
-
-    if ($zoneinfo['allow_attack'] == 'Y')
-    {
-        $attack = $langvars['l_zi_allow'];
-    }
-    else
-    {
-        $attack = $langvars['l_zi_notallow'];
-    }
-
-    if ($zoneinfo['allow_defenses'] == 'Y')
-    {
-        $defense = $langvars['l_zi_allow'];
-    }
-    elseif ($zoneinfo['allow_defenses'] == 'N')
-    {
-        $defense = $langvars['l_zi_notallow'];
-    }
-    else
-    {
-        $defense = $langvars['l_zi_limit'];
-    }
-
-    if ($zoneinfo['allow_warpedit'] == 'Y')
-    {
-        $warpedit = $langvars['l_zi_allow'];
-    }
-    elseif ($zoneinfo['allow_warpedit'] == 'N')
-    {
-        $warpedit = $langvars['l_zi_notallow'];
-    }
-    else
-    {
-        $warpedit = $langvars['l_zi_limit'];
-    }
-
-    if ($zoneinfo['allow_planet'] == 'Y')
-    {
-        $planet = $langvars['l_zi_allow'];
-    }
-    elseif ($zoneinfo['allow_planet'] == 'N')
-    {
-        $planet = $langvars['l_zi_notallow'];
-    }
-    else
-    {
-        $planet = $langvars['l_zi_limit'];
-    }
-
-    if ($zoneinfo['allow_trade'] == 'Y')
-    {
-        $trade = $langvars['l_zi_allow'];
-    }
-    elseif ($zoneinfo['allow_trade'] == 'N')
-    {
-        $trade = $langvars['l_zi_notallow'];
-    }
-    else
-    {
-        $trade = $langvars['l_zi_limit'];
-    }
-
-    if ($zoneinfo['max_hull'] == 0)
-    {
-        $hull = $langvars['l_zi_ul'];
-    }
-    else
-    {
-        $hull = $zoneinfo['max_hull'];
-    }
-
-    if (($zoneinfo['team_zone'] == 'N' && $zoneinfo['owner'] == $playerinfo['ship_id']) || ($zoneinfo['team_zone'] == 'Y' && $zoneinfo['owner'] == $playerinfo['team'] && $playerinfo['ship_id'] == $ownerinfo['creator']))
-    {
-        echo "<center>" . $langvars['l_zi_control'] .
-             ". <a href=zoneedit.php?zone=$zone>" .
-             $langvars['l_clickme'] . "</a> " . $langvars['l_zi_tochange'] .
-             "</center><p>";
-    }
-
-    echo "<table class=\"top\">\n" .
-         "<tr><td class=\"zonename\"><strong>$zoneinfo[zone_name]</strong></td></tr></table>\n" .
-         "<table class=\"bottom\">\n" .
-         "<tr><td class=\"name\">&nbsp;" . $langvars['l_zi_owner'] .
-         "</td><td class=\"value\">$ownername&nbsp;</td></tr>\n" .
-         "<tr><td>&nbsp;" . $langvars['l_beacons'] . "</td><td>$beacon&nbsp;</td></tr>\n" .
-         "<tr><td>&nbsp;" . $langvars['l_att_att'] . "</td><td>$attack&nbsp;</td></tr>\n" .
-         "<tr><td>&nbsp;" . $langvars['l_md_title'] . "</td><td>$defense&nbsp;</td></tr>\n" .
-         "<tr><td>&nbsp;" . $langvars['l_warpedit'] . "</td><td>$warpedit&nbsp;</td></tr>\n" .
-         "<tr><td>&nbsp;" . $langvars['l_planets'] . "</td><td>$planet&nbsp;</td></tr>\n" .
-         "<tr><td>&nbsp;" . $langvars['l_title_port'] . "</td><td>$trade&nbsp;</td></tr>\n" .
-         "<tr><td>&nbsp;" . $langvars['l_zi_maxhull'] . "</td><td>$hull&nbsp;</td></tr>\n" .
-         "</table>\n";
-}
-
-echo "<br><br>";
-
-Tki\Text::gotoMain($pdo_db, $lang);
-
-$footer = new Tki\Footer();
-$footer->display($pdo_db, $lang, $tkireg, $tkitimer, $template);
+            <p class="mt-1 text-sm text-white">
+                Maximum average tech level allowed:
+                <span :class="zone.over_size ? 'text-red-600' : 'text-green-600'">{{ zone.max_hull === 0 ? __('zoneinfo.l_zi_ul')  : zone.max_hull }}</span>
+            </p>
+        </div>
+    </pop-up>
+</template>
