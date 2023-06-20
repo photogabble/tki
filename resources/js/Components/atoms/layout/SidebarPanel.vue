01917d1c90a32e131d68749a56792a809eb412cc
Simon <carbontwelve@users.noreply.github.com>
2023-06-20 21:21:15 +0100

    chore: remove flex-grow on sidebar panel

--- resources/js/Components/atoms/layout/SidebarPanel.vue
+++ resources/js/Components/atoms/layout/SidebarPanel.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex-grow h-20 flex flex-col"> <!-- needs an initial height in order to grow to available screen height -->
-    <div class="uppercase border-y border-ui-orange-500/50 py-2 px-1 mb-4 border-partway-tl" style="--border-part-t-height:25%;">
+  <section>
+    <header class="flex uppercase border-y border-ui-orange-500/50 py-2 px-1 mb-4 border-partway-tl" style="--border-part-t-height:25%;">
       <slot name="heading"/>
     </div>
     <slot/>
