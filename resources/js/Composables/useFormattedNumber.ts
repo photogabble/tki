4bce7debe0ea6c5c5f7a81b57bc52a2c5ec2df86
Simon <carbontwelve@users.noreply.github.com>
2023-06-20 22:00:46 +0100

    bugfix: we use _ on backend and - on frontend :s

--- resources/js/Composables/useFormattedNumber.ts
+++ resources/js/Composables/useFormattedNumber.ts
@@ -2,6 +2,6 @@ import {useAuth} from "@/Composables/useAuth";
 
 export function useFormattedNumber() {
     const {isLoggedIn, user} = useAuth();
-    const formatter = new Intl.NumberFormat(isLoggedIn.value ? user.lang : 'en-GB');
+    const formatter = new Intl.NumberFormat(isLoggedIn.value ? user.lang.replace('_', '-') : 'en-GB');
     return (n: number) => formatter.format(n);
 }
