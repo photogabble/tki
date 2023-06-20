ea19e9813b96d49098024df13044324339dd5d06
Simon <carbontwelve@users.noreply.github.com>
2023-06-20 22:03:49 +0100

    chore: update missed closing tags

--- resources/js/Components/atoms/layout/SidebarPanel.vue
+++ resources/js/Components/atoms/layout/SidebarPanel.vue
@@ -2,7 +2,7 @@
   <section>
     <header class="flex uppercase border-y border-ui-orange-500/50 py-2 px-1 mb-4 border-partway-tl" style="--border-part-t-height:25%;">
       <slot name="heading"/>
-    </div>
+    </header>
     <slot/>
-  </div>
+  </section>
 </template>
