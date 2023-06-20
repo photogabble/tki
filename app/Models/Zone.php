68ede5b9becccce2478726336e8cf663b6b31912
Simon <carbontwelve@users.noreply.github.com>
2023-06-20 21:33:05 +0100

    refactor: moved to ZoneResource

--- app/Models/Zone.php
+++ app/Models/Zone.php
@@ -80,67 +80,6 @@ class Zone extends Model
     }
 
     /**
-     * Returns ZoneInfo from the perspective of User.
-     * Refactored from zoneinfo.php.
-     *
-     * @param User $user
-     * @return ZoneInfo
-     */
-    public function info(User $user): ZoneInfo
-    {
-        $name = ($this->id < 5)
-            ? __('common.l_zname_'. $this->id)
-            : $this->name;
-
-        switch($this->id) {
-            case 1:
-                $owner = __('zoneinfo.l_zi_nobody');
-                break;
-            case 2:
-                $owner = __('zoneinfo.l_zi_feds');
-                break;
-            case 3:
-                $owner = __('zoneinfo.l_zi_traders');
-                break;
-            case 4:
-                $owner = __('zoneinfo.l_zi_war');
-                break;
-            default:
-                $owner = $this->owner->name;
-        }
-
-        $userCanEdit = false;
-
-        if (
-            ($this->team_zone && $user->team_id === $this->owner_id && $this->owner->creator === $user->id) ||
-            (!$this->team_zone && $user->id === $this->owner_id)
-        ) {
-            $userCanEdit = true;
-        }
-
-        // TODO: implement isFriendly:
-        //       Y: No danger, not a War Zone
-        //       L: Is a War Zone, in a sector we had a battle
-        //       N: Owned by team / player we are hostile towards
-
-        return new ZoneInfo(
-            $name,
-            $owner,
-            $userCanEdit,
-            ZonePermission::Allow,
-            $this->allow_beacon,
-            $this->allow_attack,
-            $this->allow_planetattack,
-            $this->allow_warpedit,
-            $this->allow_planet,
-            $this->allow_trade,
-            $this->allow_defenses,
-            $this->max_hull,
-            $user->ship->hull > $this->max_hull,
-        );
-    }
-
-    /**
      * @param int $sector_id
      * @return Zone|null
      *@todo refactor usages to be Model aware
