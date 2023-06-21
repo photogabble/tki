a6e69ca559af2f181b350bf8cccccf604953d684
Simon <carbontwelve@users.noreply.github.com>
2023-06-21 01:11:21 +0100

    feat: add up-down indicator

---
+++ resources/js/Components/atoms/UpDownIndicator.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+withDefaults(defineProps<{
+  direction: 'ASC'|'DESC'
+}>(), {
+  direction: 'DESC',
+});
+
+</script>
+
+<template>
+  <span v-if="direction === 'ASC'">▲</span>
+  <span v-if="direction === 'DESC'">▼</span>
+</template>
