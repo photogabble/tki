da377e1bff91296dbdcb0e2f68dc76c9674a8de5
Simon <carbontwelve@users.noreply.github.com>
2023-06-21 01:13:13 +0100

    feat: add table column sorting link component

---
+++ resources/js/Components/molecules/ColumnSortLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {Link} from "@inertiajs/vue3";
+import UpDownIndicator from "@/Components/atoms/UpDownIndicator.vue";
+withDefaults(defineProps<{
+  href: string
+  direction: 'ASC' | 'DESC',
+  isSorting: boolean,
+}>(), {
+  href: '#',
+  direction: 'DESC',
+  isSorting: false,
+});
+</script>
+
+<template>
+  <Link :href="href" :class="`${isSorting ? 'text-ui-yellow' : 'hover:text-ui-yellow' }`">
+    <slot/> <up-down-indicator v-if="isSorting" :direction="direction" />
+  </Link>
+</template>
