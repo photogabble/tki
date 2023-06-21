02acf63a6635a45370e7c3744d327744e2990df3
Simon <carbontwelve@users.noreply.github.com>
2023-06-21 01:13:41 +0100

    feat: add player and team ranking tables

---
+++ resources/js/Components/organisms/PlayerRanking.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import {usePage, Link} from "@inertiajs/vue3";
+import type {RankingPageProps, PlayerRankingParams} from "@/types/ranking";
+import SectionHeader from "@/Components/atoms/layout/SectionHeader.vue";
+import ColumnSortLink from "@/Components/molecules/ColumnSortLink.vue";
+
+const {ranking, sorts, sorting_by, sorting_direction}  = usePage<RankingPageProps>().props.player;
+const linkParams = (s: string) => {
+  const ret : PlayerRankingParams = {sort_players_by: s};
+  if (sorting_by === s) {
+    ret.sort_players_direction = sorting_direction === 'ASC'
+      ? 'DESC'
+      : 'ASC'
+  }
+
+  return ret;
+}
+
+</script>
+
+<template>
+  <section>
+    <section-header>
+      <template #actions>
+        <span class="mr-2">Page {{ ranking.meta.current_page }}/{{ ranking.meta.last_page }}</span>
+        <nav class="space-x-2">
+          <Link v-for="link of ranking.meta.links" :class="`${link.active ? 'text-ui-yellow' : 'hover:text-ui-yellow' }`" :href="link.url" v-html="link.label"/>
+        </nav>
+      </template>
+      <span class="text-white">Players Ranking</span>
+    </section-header>
+
+    <table class="w-full">
+      <thead>
+        <tr class="border-b">
+          <th class="p-1 text-left">Rank</th>
+          <th class="p-1 text-left text-white">
+            <column-sort-link :href="route('ranking', linkParams('score'))" :is-sorting="sorting_by === 'score'" :direction="sorting_direction">
+              Score
+            </column-sort-link>
+          </th>
+          <th class="p-1 text-left">Player</th>
+          <th class="p-1 text-left text-white">
+            <column-sort-link :href="route('ranking', linkParams('turns'))" :is-sorting="sorting_by === 'turns'" :direction="sorting_direction">
+              Turns Used
+            </column-sort-link>
+          </th>
+          <th class="p-1 text-left text-white">
+            <column-sort-link :href="route('ranking', linkParams('login'))" :is-sorting="sorting_by === 'login'" :direction="sorting_direction">
+              Last Login
+            </column-sort-link>
+          </th>
+          <th class="p-1 text-left text-white">
+            <Link :href="route('ranking', {sort_players_by: 'good'})" :class="`${sorting_by === 'good' ? 'text-ui-yellow' : 'hover:text-ui-yellow' }`">Good</Link>/<Link :href="route('ranking', {sort_players_by: 'bad'})" :class="`${sorting_by === 'bad' ? 'text-ui-yellow' : 'hover:text-ui-yellow' }`">Evil</Link>
+          </th>
+          <th class="p-1 text-left text-white">
+            <column-sort-link :href="route('ranking', linkParams('efficiency'))" :is-sorting="sorting_by === 'efficiency'" :direction="sorting_direction">
+              Eff. Rating
+            </column-sort-link>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="player in ranking.data">
+          <td class="p-1">123</td>
+          <td class="p-1">{{ player.score }}</td>
+          <td class="p-1">{{ player.name }}</td>
+          <td class="p-1">{{ player.turns_used }}</td>
+          <td class="p-1">{{ player.last_login.nice }}</td>
+          <td :class="['p-1', {'text-red-600': player.rating < 0, 'text-green-600': player.rating > 0}]">{{ player.rating }}</td>
+          <td class="p-1">{{player.efficiency}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p>Total number of players: {{ ranking.meta.total }}. Players with destroyed ships are not counted.</p>
+  </section>
+</template>
---
+++ resources/js/Components/organisms/TeamRanking.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {usePage, Link} from "@inertiajs/vue3";
+import type {RankingPageProps} from "@/types/ranking";
+import SectionHeader from "@/Components/atoms/layout/SectionHeader.vue";
+</script>
+
+<template>
+  <section>
+    <section-header>
+      <template #actions>
+        TODO
+      </template>
+      <span class="text-white">Teams Ranking</span>
+    </section-header>
+
+    <table class="w-full">
+      <thead>
+      <tr class="border-b">
+        <th class="p-1 text-left text-white">Rank</th>
+        <th class="p-1 text-left text-white">Score</th>
+        <th class="p-1 text-left text-white">Team Name</th>
+        <th class="p-1 text-left text-white"># Players</th>
+        <th class="p-1 text-left text-white">Last Login</th>
+        <th class="p-1 text-left text-white">Good/Evil</th>
+        <th class="p-1 text-left text-white">Eff. Rating</th>
+      </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+
+  </section>
+</template>
