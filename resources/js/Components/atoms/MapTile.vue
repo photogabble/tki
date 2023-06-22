3ed8c12a73c56866afe831d17777136fb655a48f
Simon <carbontwelve@users.noreply.github.com>
2023-06-22 23:16:59 +0100

    feat: add map tile component

---
+++ resources/js/Components/atoms/MapTile.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import type {SectorType} from "@/types/resources/sector";
+import {computed} from "vue";
+
+const props = withDefaults(
+    defineProps<{
+      type: SectorType,
+    }>(),
+    {
+      type: 'unknown',
+    }
+);
+
+const imageSrc = computed(() => {
+  const filename = (['none', 'unknown'].includes(props.type))
+      ? props.type
+      : `port-${props.type}`;
+
+  return new URL(`../../../images/map-tiles/${filename}.png`, import.meta.url)
+});
+</script>
+
+<template>
+  <img :src="imageSrc"/>
+</template>
