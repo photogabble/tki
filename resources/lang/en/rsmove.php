ea3af88ff979525db2e855d4b79a22773bb2c450
Simon <carbontwelve@users.noreply.github.com>
2023-06-24 16:52:51 +0100

    chore: replace [var] with :var

--- resources/lang/en/rsmove.php
+++ resources/lang/en/rsmove.php
@@ -1,13 +1,13 @@
 <?php return [
     "l_rs_title" => "Realspace move",
     "l_rs_whichsector" => "Which sector would you like to reach through realspace?",
-    "l_rs_insector" => "You are presently in sector [sector] - and there are sectors available from 1 to [max_sectors].",
+    "l_rs_insector" => "You are presently in sector :sector - and there are sectors available from 1 to :max_sectors.",
     "l_rs_submit" => "Compute",
-    "l_rs_movetime" => "With your engines, it will take [triptime] turns to complete the journey.",
-    "l_rs_energy" => "You would gather [energy] units of energy",
+    "l_rs_movetime" => "With your engines, it will take :triptime turns to complete the journey.",
+    "l_rs_energy" => "You would gather :energy units of energy.",
     "l_rs_noturns" => "You do not have enough turns left, and cannot embark on this journey.",
     "l_rs_engage_link" => "Engage",
-    "l_rs_engage" => "You have [turns] turns. [engage] engines?",
+    "l_rs_engage" => "You have :turns turns. Engage engines?",
     "l_rs_invalid" => "Invalid destination",
-    "l_rs_ready" => "You are now in sector [sector]. You used [triptime] turns, and gained [energy] energy units.",
+    "l_rs_ready" => "You are now in sector :sector. You used :triptime turns, and gained :energy energy units.",
 ];
