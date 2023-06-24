b16d2e41d9e3532b4d63a609935029a729cf6069
Simon <carbontwelve@users.noreply.github.com>
2023-06-24 16:57:25 +0100

    feat: add toSimpleArray query builder macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Tki\Providers;
 
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +24,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        // toSimpleArray macro obtained from article written by Eelco Luurtsema.
+        // @see https://medium.com/@eelcoluurtsema/improve-performance-of-laravels-pagination-with-complex-queries-e266a5bb33eb
+        QueryBuilder::macro('toSimpleArray', function ($column) {
+            $results = $this->onceWithColumns(Arr::wrap($column), function () {
+                return $this->processor->processSelect($this, $this->runSelect());
+            });
+
+            return array_map(function ($item) use ($column) {
+                return $item->{$column};
+            }, $results);
+        });
+
+        EloquentBuilder::macro('toSimpleArray', function ($column) {
+            return $this->query->toSimpleArray($column);
+        });
     }
 }
