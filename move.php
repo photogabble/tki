8f7d19c24683154206cafbef5882b1d3245ced54
Simon <carbontwelve@users.noreply.github.com>
2023-06-26 22:42:43 +0100

    refactor: move.php -> WarpNavigationController

--- move.php
+++ move.php
@@ -22,95 +22,66 @@
  *
  */
 
-require_once './common.php';
+namespace Tki\Http\Controllers;
 
-$login = new Tki\Login();
-$login->checkLogin($pdo_db, $lang, $tkireg, $tkitimer, $template);
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Tki\Http\Resources\SectorResource;
+use Tki\Models\Universe;
+use Tki\Models\User;
+use Tki\Types\MovementMode;
 
-// Database driven language entries
-$langvars = Tki\Translate::load($pdo_db, $lang, array('combat', 'footer',
-                                'insignias', 'move', 'news', 'universal'));
-$title = $langvars['l_move_title'];
+class WarpNavigationController extends Controller {
+    public function makeWarpMove(Request $request): JsonResponse {
+        $this->validate($request, [
+            'sector' => ['required', 'exists:universes,id'],
+        ]);
 
-$header = new Tki\Header();
-$header->display($pdo_db, $lang, $template, $title);
+        /** @var User $user */
+        $user = $request->user();
+        $sector = $request->get('sector');
 
-$sector = (int) filter_input(INPUT_GET, 'sector', FILTER_SANITIZE_NUMBER_INT);
+        // Check to see if the player has less than one turn available
+        // and if so return 412
+        if ($user->turns < 1) {
+            return new JsonResponse([
+                'message' => __('move.l_move_turn')
+            ], 412);
+        }
 
-// Get playerinfo from database
-$players_gateway = new \Tki\Models\User($pdo_db);
-$playerinfo = $players_gateway->selectPlayerInfo($_SESSION['username']);
+        // Retrieve the warp link out of the current sector
+        $link = $user->sector->links()
+            ->where('dest', $sector)
+            ->first();
 
-// Check to see if the player has less than one turn available
-// and if so return to the main menu
-if ($playerinfo['turns'] < 1)
-{
-    echo $langvars['l_move_turn'] . '<br><br>';
-    Tki\Text::gotoMain($pdo_db, $lang);
+        if (!$link) {
+            return new JsonResponse([
+                'message' => __('move.l_move_failed')
+            ], 400);
+        }
 
-    $footer = new Tki\Footer();
-    $footer->display($pdo_db, $lang, $tkireg, $tkitimer, $template);
-    die();
-}
+        // TODO: refactor CheckDefenses::fighters to provide a JsonResponse on battle. This can sometimes result in
+        //       the player _not_ making it to their final destination!
+        // \Tki\CheckDefenses::fighters($pdo_db, $lang, $sector, $playerinfo, $tkireg, $title, $calledfrom);
 
-// Get sectorinfo from database
-$sectors_gateway = new \Tki\Models\Universe($pdo_db);
-$sectorinfo = $sectors_gateway->selectSectorInfo($playerinfo['sector']);
+        // TODO: make warp turns dependant upon ship clasification
+        $user->decrement('turns', 1);
+        $user->increment('turns_used', 1);
 
-// Retrive all the warp links out of the current sector
-$result3 = $old_db->Execute("SELECT * FROM {$old_db->prefix}links WHERE link_start = ?;", array($playerinfo['sector']));
-Tki\Db::logDbErrors($pdo_db, $result3, __LINE__, __FILE__);
-$i = 0;
-$flag = 0;
+        $user->ship->travelTo($sector, MovementMode::Warp, 1, 0);
 
-// Loop through the available warp links to make sure it's a valid move
-while (!$result3->EOF)
-{
-    $row = $result3->fields;
-    if ($row['link_dest'] == $sector && $row['link_start'] == $playerinfo['sector'])
-    {
-        $flag = 1;
+        // TODO: refactor CheckDefenses::mines to provide a JsonResponse on battle
+        // \Tki\CheckDefenses::mines($pdo_db, $lang, $sector, $title, $playerinfo, $tkireg);
+
+        $destination = Universe::queryForUser($user)->find($sector);
+        return new JsonResponse([
+            'sector' => new SectorResource($destination),
+            'turns' => 1,
+            'energy_scooped' => 0,
+        ]);
     }
 
-    $i++;
-    $result3->MoveNext();
-}
-
-// Check if there was a valid warp link to move to
-if ($flag == 1)
-{
-    $calledfrom = "move.php";
-    Tki\CheckDefenses::fighters($pdo_db, $lang, $sector, $playerinfo, $tkireg, $title, $calledfrom);
-
-    $cur_time_stamp = date("Y-m-d H:i:s");
-    \Tki\Models\MovementLog::writeLog($pdo_db, $playerinfo['ship_id'], $sector);
-    $move_result = $old_db->Execute("UPDATE {$old_db->prefix}ships " .
-                                "SET last_login = ?, " .
-                                "turns = turns - 1, " .
-                                "turns_used = turns_used + 1, " .
-                                "sector = ? WHERE ship_id = ?;",
-                                array($cur_time_stamp, $sector, $playerinfo['ship_id']));
-    Tki\Db::logDbErrors($pdo_db, $move_result, __LINE__, __FILE__);
-    if (!$move_result)
-    {
-        // Is this really STILL needed?
-        $error = $old_db->ErrorMsg();
-        mail($tkireg->admin_mail, "Move Error", "Start Sector: $sectorinfo[sector_id]\n" .
-            "End Sector: $sector\nPlayer: $playerinfo[character_name] - " .
-            $playerinfo['ship_id'] . "\n\nQuery:  $query\n\nSQL error: $error");
+    public function calculateWarpMoves(Request $request): JsonResponse {
+        // TODO: Refactor navcomp.php into action and invoke here
     }
-
-    // Enter code for checking dangers in new sector
-    Tki\CheckDefenses::mines($pdo_db, $lang, $sector, $title, $playerinfo, $tkireg);
-    header("Location: main.php");
 }
-else
-{
-    echo $langvars['l_move_failed'] . '<br><br>';
-    $resx = $old_db->Execute("UPDATE {$old_db->prefix}ships SET cleared_defenses=' ' " .
-                         "WHERE ship_id = ?;", array($playerinfo['ship_id']));
-    Tki\Db::logDbErrors($pdo_db, $resx, __LINE__, __FILE__);
-    Tki\Text::gotoMain($pdo_db, $lang);
-}
-
-echo "</body></html>";
