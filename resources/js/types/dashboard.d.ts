08b727c560a5b863f90f54a736923c153cdc1a0e
Simon <carbontwelve@users.noreply.github.com>
2023-06-27 22:14:32 +0100

    chore: tidy dashboard type declaration file

--- resources/js/types/dashboard.d.ts
+++ resources/js/types/dashboard.d.ts
@@ -1,8 +1,5 @@
 import {PageProps as InertiaPageProps} from "@inertiajs/core";
 
-export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-    // TODO
-}
+export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {}
 
-export interface DashboardPageProps extends InertiaPageProps, PageProps {
-}
+export interface DashboardPageProps extends InertiaPageProps, PageProps {}
