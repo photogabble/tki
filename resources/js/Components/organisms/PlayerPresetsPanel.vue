dcdab3962f8a43edf90d7f0da4cbd7af7d6b08ac
Simon <carbontwelve@users.noreply.github.com>
2023-06-27 22:17:24 +0100

    feat: use preset editor popup

--- resources/js/Components/organisms/PlayerPresetsPanel.vue
+++ resources/js/Components/organisms/PlayerPresetsPanel.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import NavigationConfirmPopup from "@/Components/organisms/NavigationConfirmPopup.vue";
+import PresetEditorPopup from "@/Components/organisms/PresetEditorPopup.vue";
 import SidebarPanel from "@/Components/atoms/layout/SidebarPanel.vue";
 import SectorNavButton from "@/Components/atoms/SectorNavButton.vue";
+import TextButton from "@/Components/atoms/form/TextButton.vue";
 import {PresetResource} from "@/types/resources/preset";
 import {ref} from "vue";
 
-const selectedSector = ref(-1);
+const selectedSector = ref<number>(-1);
+const editingPreset = ref<PresetResource|undefined>();
 
 const props = defineProps<{
   presets: Array<PresetResource>;
@@ -15,18 +18,19 @@ const props = defineProps<{
 
 <template>
   <navigation-confirm-popup v-model="selectedSector" />
+  <preset-editor-popup v-model="editingPreset" />
   <sidebar-panel>
     <template #heading>
-      <span class="text-white">Realspace</span>
+      <span class="text-white">{{ __('common.l_realspace') }}</span>
     </template>
     <section class="flex flex-col">
       <ul>
-        <li v-for="preset in presets" class="flex">
+        <li v-for="preset in presets" class="flex" :key="`realspace-preset-${preset.id}`">
           <sector-nav-button :link="preset.link" @click="selectedSector = preset.link.to_sector_id" />
-          <button>[Set]</button>
+          <text-button @click="editingPreset = preset">[Set]</text-button>
         </li>
       </ul>
-      <button @click="selectedSector = 0" class="self-end hover:text-white">[Other]</button>
+      <text-button @click="selectedSector = 0" class="self-end">[{{ __('main.l_main_other') }}]</text-button>
     </section>
   </sidebar-panel>
 </template>
