57d5322d4d539ab4d5e98fd45cb16321130623ce
Simon <carbontwelve@users.noreply.github.com>
2023-06-28 22:10:11 +0100

    chore: update lang for navcom

--- resources/lang/en/navcomp.php
+++ resources/lang/en/navcomp.php
@@ -3,7 +3,6 @@
     "l_nav_nocomp" => "Navigation computer is not available",
     "l_nav_query" => "Enter sector to find path to:",
     "l_nav_pathfnd" => "Path found",
-    "l_nav_answ1" => "It will take you",
-    "l_nav_answ2" => "turns to get to this sector.",
+    "l_nav_answ" => "It will take you :turns turns to get to this sector.",
     "l_nav_proper" => "Your computer technology is too primitive to compute a warp route to that distance.",
 ];
