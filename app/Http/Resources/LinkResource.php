c5b210036f33e4c77479254ef2282d92b84c50bd
Simon <carbontwelve@users.noreply.github.com>
2023-06-28 22:10:56 +0100

    refactor: LinkResource can wrap either Universe OR Link Models

--- app/Http/Resources/LinkResource.php
+++ app/Http/Resources/LinkResource.php
@@ -5,10 +5,11 @@ namespace Tki\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Tki\Models\Link;
+use Tki\Models\Universe;
 use Tki\Models\User;
 
 /**
- * @mixin Link
+ * @mixin Link|Universe
  */
 class LinkResource extends JsonResource
 {
@@ -22,10 +23,20 @@ class LinkResource extends JsonResource
         /** @var User $user */
         $user = $request->user();
 
-        return [
-            'to_sector_id' => $this->dest,
-            'hasVisited' => $user->hasVisitedSector($this->dest),
-            'hasDanger' => false, // TODO: Implement hasDanger, maybe link with Zone Resource isFriendly
-        ];
+        if ($this->resource instanceof Link) {
+            return [
+                'to_sector_id' => $this->dest,
+                'hasVisited' => $user->hasVisitedSector($this->dest),
+                'hasDanger' => false, // TODO: Implement hasDanger, maybe link with Zone Resource isFriendly
+            ];
+        }
+
+        if ($this->resource instanceof Universe) {
+            return [
+                'to_sector_id' => $this->id,
+                'hasVisited' => $this->has_visited,
+                'hasDanger' => false,
+            ];
+        }
     }
 }
