713c38cf368fbf0d567667c80eb84c1d848d52c9
Simon <carbontwelve@users.noreply.github.com>
2023-07-05 21:59:56 +0100

    refactor: Score action class to use Eloquent

--- app/Actions/Score.php
+++ app/Actions/Score.php
@@ -24,11 +24,13 @@
 
 namespace Tki\Actions;
 
-use Tki\Registry;
+use Illuminate\Support\Facades\DB;
+use Tki\Models\BankAccount;
+use Tki\Models\User;
 
 class Score
 {
-    public static function updateScore(\PDO $pdo_db, int $ship_id, Registry $tkireg, array $playerinfo): int
+    public static function updateScore(User $user): int
     {
         // Not currently used in calculation!
         // $base_ore = $tkireg->base_ore;
@@ -36,73 +38,64 @@ class Score
         // $base_organics = $tkireg->base_organics;
 
         // These are all SQL Queries, so treat them like them.
-        $calc_hull              = "ROUND(POW(" . $tkireg->upgrade_factor . ", hull))";
-        $calc_engines           = "ROUND(POW(" . $tkireg->upgrade_factor . ", engines))";
-        $calc_power             = "ROUND(POW(" . $tkireg->upgrade_factor . ", power))";
-        $calc_computer          = "ROUND(POW(" . $tkireg->upgrade_factor . ", computer))";
-        $calc_sensors           = "ROUND(POW(" . $tkireg->upgrade_factor . ", sensors))";
-        $calc_beams             = "ROUND(POW(" . $tkireg->upgrade_factor . ", beams))";
-        $calc_torp_launchers    = "ROUND(POW(" . $tkireg->upgrade_factor . ", torp_launchers))";
-        $calc_shields           = "ROUND(POW(" . $tkireg->upgrade_factor . ", shields))";
-        $calc_armor             = "ROUND(POW(" . $tkireg->upgrade_factor . ", armor))";
-        $calc_cloak             = "ROUND(POW(" . $tkireg->upgrade_factor . ", cloak))";
-        $calc_levels            = "($calc_hull + $calc_engines + $calc_power + $calc_computer + $calc_sensors + $calc_beams + $calc_torp_launchers + $calc_shields + $calc_armor + $calc_cloak) * " . $tkireg->upgrade_cost;
+        $calc_hull              = "ROUND(POW(" . config('game.upgrade_factor') . ", hull))";
+        $calc_engines           = "ROUND(POW(" . config('game.upgrade_factor') . ", engines))";
+        $calc_power             = "ROUND(POW(" . config('game.upgrade_factor') . ", power))";
+        $calc_computer          = "ROUND(POW(" . config('game.upgrade_factor') . ", computer))";
+        $calc_sensors           = "ROUND(POW(" . config('game.upgrade_factor') . ", sensors))";
+        $calc_beams             = "ROUND(POW(" . config('game.upgrade_factor') . ", beams))";
+        $calc_torp_launchers    = "ROUND(POW(" . config('game.upgrade_factor') . ", torp_launchers))";
+        $calc_shields           = "ROUND(POW(" . config('game.upgrade_factor') . ", shields))";
+        $calc_armor             = "ROUND(POW(" . config('game.upgrade_factor') . ", armor))";
+        $calc_cloak             = "ROUND(POW(" . config('game.upgrade_factor') . ", cloak))";
+        $calc_levels            = "($calc_hull + $calc_engines + $calc_power + $calc_computer + $calc_sensors + $calc_beams + $calc_torp_launchers + $calc_shields + $calc_armor + $calc_cloak) * " . config('game.upgrade_cost');
 
-        $calc_torps             = "::prefix::ships.torps * " . $tkireg->torpedo_price;
-        $calc_armor_pts         = "armor_pts * " . $tkireg->armor_price;
-        $calc_ship_ore          = "ship_ore * " . $tkireg->ore_price;
-        $calc_ship_organics     = "ship_organics * " . $tkireg->organics_price;
-        $calc_ship_goods        = "ship_goods * " . $tkireg->goods_price;
-        $calc_ship_energy       = "ship_energy * " . $tkireg->energy_price;
-        $calc_ship_colonists    = "ship_colonists * " . $tkireg->colonist_price;
-        $calc_ship_fighters     = "ship_fighters * " . $tkireg->fighter_price;
+        $calc_torps             = "ships.torps * " . config('game.torpedo_price');
+        $calc_armor_pts         = "armor_pts * " . config('game.armor_price');
+        $calc_ship_ore          = "ship_ore * " . config('game.ore_price');
+        $calc_ship_organics     = "ship_organics * " . config('game.organics_price');
+        $calc_ship_goods        = "ship_goods * " . config('game.goods_price');
+        $calc_ship_energy       = "ship_energy * " . config('game.energy_price');
+        $calc_ship_colonists    = "ship_colonists * " . config('game.colonist_price');
+        $calc_ship_fighters     = "ship_fighters * " . config('game.fighter_price');
         $calc_equip             = "$calc_torps + $calc_armor_pts + $calc_ship_ore + $calc_ship_organics + $calc_ship_goods + $calc_ship_energy + $calc_ship_colonists + $calc_ship_fighters";
 
-        $calc_dev_warpedit      = "dev_warpedit * " . $tkireg->dev_warpedit_price;
-        $calc_dev_genesis       = "dev_genesis * " . $tkireg->dev_genesis_price;
-        $calc_dev_beacon        = "dev_beacon * " . $tkireg->dev_beacon_price;
-        $calc_dev_emerwarp      = "dev_emerwarp * " . $tkireg->dev_emerwarp_price;
-        $calc_dev_escapepod     = "IF(dev_escapepod='Y', " . $tkireg->dev_escapepod_price . ", 0)";
-        $calc_dev_fuelscoop     = "IF(dev_fuelscoop='Y', " . $tkireg->dev_fuelscoop_price . ", 0)";
-        $calc_dev_lssd          = "IF(dev_lssd='Y', " . $tkireg->dev_lssd_price . ", 0)";
-        $calc_minedeflector     = "dev_minedeflector * " . $tkireg->dev_minedeflector_price;
+        $calc_dev_warpedit      = "dev_warpedit * " . config('game.dev_warpedit_price');
+        $calc_dev_genesis       = "dev_genesis * " . config('game.dev_genesis_price');
+        $calc_dev_beacon        = "dev_beacon * " . config('game.dev_beacon_price');
+        $calc_dev_emerwarp      = "dev_emerwarp * " . config('game.dev_emerwarp_price');
+        $calc_dev_escapepod     = "IF(dev_escapepod=true, " . config('game.dev_escapepod_price') . ", 0)";
+        $calc_dev_fuelscoop     = "IF(dev_fuelscoop=true, " . config('game.dev_fuelscoop_price') . ", 0)";
+        $calc_dev_lssd          = "IF(dev_lssd=true, " . config('game.dev_lssd_price') . ", 0)";
+        $calc_minedeflector     = "dev_minedeflector * " . config('game.dev_minedeflector_price');
         $calc_dev               = "$calc_dev_warpedit + $calc_dev_genesis + $calc_dev_beacon + $calc_dev_emerwarp + $calc_dev_escapepod + $calc_dev_fuelscoop + $calc_minedeflector + $calc_dev_lssd";
 
-        $calc_planet_goods      = "SUM(::prefix::planets.organics) * " . $tkireg->organics_price . "+ SUM(::prefix::planets.ore) * " . $tkireg->ore_price . "+ SUM(::prefix::planets.goods) * " . $tkireg->goods_price . "+ SUM(::prefix::planets.energy) * " . $tkireg->energy_price;
-        $calc_planet_cols       = "SUM(::prefix::planets.colonists) * " . $tkireg->colonist_price;
-        $calc_planet_defense    = "SUM(::prefix::planets.fighters) * " . $tkireg->fighter_price . "+ IF(::prefix::planets.base='Y', " . $tkireg->base_credits . "+ SUM(::prefix::planets.torps) * " . $tkireg->torpedo_price . ", 0)";
-        $calc_planet_credits    = "SUM(::prefix::planets.credits)";
+        $calc_planet_goods      = "SUM(planets.organics) * " . config('game.organics_price') . "+ SUM(planets.ore) * " . config('game.ore_price') . "+ SUM(planets.goods) * " . config('game.goods_price') . "+ SUM(planets.energy) * " . config('game.energy_price');
+        $calc_planet_cols       = "SUM(planets.colonists) * " . config('game.colonist_price');
+        $calc_planet_defense    = "SUM(planets.fighters) * " . config('game.fighter_price') . "+ IF(planets.base='Y', " . config('game.base_credits') . "+ SUM(planets.torps) * " . config('game.torpedo_price') . ", 0)";
+        $calc_planet_credits    = "SUM(planets.credits)";
 
-        $sql = "SELECT IF(COUNT(*)>0, $calc_planet_goods + $calc_planet_cols + $calc_planet_defense + $calc_planet_credits, 0) AS planet_score " .
-                                     "FROM ::prefix::planets WHERE owner = :ship_id GROUP BY planet_id";
-        $stmt = $pdo_db->prepare($sql);
-        $stmt->bindParam(':ship_id', $ship_id, \PDO::PARAM_INT);
-        $stmt->execute();
-        $planet_score = $stmt->fetch(\PDO::FETCH_COLUMN);
+        $planet = DB::table('planets')
+            ->selectRaw("SELECT IF(COUNT(*)>0, $calc_planet_goods + $calc_planet_cols + $calc_planet_defense + $calc_planet_credits, 0) AS score")
+            ->where('owner_id', $user->id)
+            ->groupBy('id')
+            ->first();
 
-        $sql = "SELECT IF(COUNT(*)>0, $calc_levels + $calc_equip + $calc_dev + ::prefix::ships.credits, 0) AS ship_score " .
-               "FROM ::prefix::ships LEFT JOIN ::prefix::planets ON ::prefix::planets.owner=ship_id WHERE ship_id = :ship_id AND destroyed_at IS NULL GROUP BY ship_id";
-        $stmt = $pdo_db->prepare($sql);
+        $ship = DB::table('ships')
+            ->selectRaw("SELECT IF(COUNT(*)>0, $calc_levels + $calc_equip + $calc_dev + ships.credits, 0) AS score")
+            ->join('planets', 'planets.owner_id', '=', 'ships.owner_id')
+            ->where('owner_id', $user->id)
+            ->whereNull('destroyed_at')
+            ->groupBy('ships.id')
+            ->first();
 
-        $stmt->bindParam(':ship_id', $ship_id, \PDO::PARAM_INT);
-        $stmt->execute();
-        $ship_score = $stmt->fetch(\PDO::FETCH_COLUMN);
-
-        $ibank_gateway = new Tki\Models\BankAccount($pdo_db);
-        $bank_score = $ibank_gateway->selectIbankScore($playerinfo['ship_id']);
-
-        $score = $ship_score + $planet_score + $bank_score;
-        if ($score < 0)
-        {
-            $score = 0;
-        }
-
+        $score = $ship->score + $planet->score + (new BankAccount)->selectIbankScore($user->id);
+        if ($score < 0) $score = 0;
         $score = (int) round(sqrt($score));
-        $stmt = $pdo_db->prepare("UPDATE ::prefix::ships SET score = :score WHERE ship_id = :ship_id");
-        $stmt->bindParam(':score', $score, \PDO::PARAM_INT);
-        $stmt->bindParam(':ship_id', $playerinfo['ship_id'], \PDO::PARAM_INT);
-        $result = $stmt->execute();
-        \Tki\Db::logDbErrors($pdo_db, $result, __LINE__, __FILE__);
+
+        $user->update([
+            'score' => $score,
+        ]);
 
         return $score;
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -63,7 +63,8 @@ class User extends Authenticatable
         'turns_used',
         'turns',
         'credits',
-        'rating'
+        'rating',
+        'score',
     ];
 
     /**
@@ -84,7 +85,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
-        'last_login' => 'datetime'
+        'last_login' => 'datetime',
     ];
 
     /**
